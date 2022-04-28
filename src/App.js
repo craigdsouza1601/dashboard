5fe00b84539f65d0bc4f809da3faf111a67dbdac
craigdsouza1601 <craig1601@gmail.com>
2022-04-29 03:06:07 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './assets/boxicons-2.1.2/css/boxicons.min.css'
+import './App.css';
+import './grid.css'
+
+import Layout from './components/layout/Layout'
+
+const App = () => {
+  return (
+    <div>
+        <Layout />
+    </div>
+  )
+}
+
+export default App
